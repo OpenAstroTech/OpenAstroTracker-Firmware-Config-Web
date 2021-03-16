86641049c47830bc7a8ff8fd084c5842e3643264
Chris <christian.kardach@gmail.com>
2021-03-16 18:38:48 +0100

    Updated intro page

--- src/modules/Home.jsx
+++ src/modules/Home.jsx
@@ -2,21 +2,22 @@ import React from 'react';
 
 const Home = (props) => {
 
-    return <div>
-          <a href="steps">
-            <img src="/images/oat_banner.png" />
-          </a>
-          <p>
-            Click the OAT to get started.
-        </p>
-          <a
-            href="https://wiki.openastrotech.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            OAT Wiki
-        </a>
+  return (
+    <div className="bg-dim full-bg-size"
+      style={{
+        backgroundImage: `url("/images/oat_bkg.jpg")`,
+        padding: `0px`
+      }}>
+
+      <div className="intro-horizontal-bar">
+        <div className="intro-link-child">
+          <h3 className="intro-header">OAT FW CONFIGURATION</h3>
+          <a className="intro-link" href="steps">START</a>
+        </div>
+      </div>
+
     </div>
+  );
 }
 
 export default Home;
